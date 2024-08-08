290ba5b69cfa0c5ad61eea88c44f082032571d46
PRAVEEN-KUMAR-M4S <praveenkumarotp3@gmail.com>
2024-08-08 12:43:16 +0530

    screat key

--- lib/core/secrets/keys.dart
+++ lib/core/secrets/keys.dart
@@ -1,3 +1,6 @@
 class Keys {
+  static const String apiKey =
+      "https://intent-kit-16.hasura.app/api/rest/blogs";
 
+  static const String adminSecret = "32qR4KmXOIpsGPQKMqEJHGJS27G5s7HdSKO3gdtQd2kv5e852SiYwWNfxkZOBuQ6";
 }
--- lib/faetures/blog/data/datasources/blog_remote_data_source.dart
+++ lib/faetures/blog/data/datasources/blog_remote_data_source.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:blog_app/core/error/exceptions.dart';
+import 'package:blog_app/core/secrets/keys.dart';
 import 'package:blog_app/faetures/blog/data/models/blog_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -10,13 +11,10 @@ abstract interface class BlogRemoteDataSource {
 class BlogRemoteDataSourceImp implements BlogRemoteDataSource {
   @override
   Future<List<BlogModel>> getAllBlogs() async {
-    const String url = 'https://intent-kit-16.hasura.app/api/rest/blogs';
-    const String adminSecret =
-        '32qR4KmXOIpsGPQKMqEJHGJS27G5s7HdSKO3gdtQd2kv5e852SiYwWNfxkZOBuQ6';
 
     try {
-      final response = await http.get(Uri.parse(url), headers: {
-        'x-hasura-admin-secret': adminSecret,
+      final response = await http.get(Uri.parse(Keys.apiKey), headers: {
+        'x-hasura-admin-secret': Keys.adminSecret,
       });
 
       var responseData = json.decode(response.body);
